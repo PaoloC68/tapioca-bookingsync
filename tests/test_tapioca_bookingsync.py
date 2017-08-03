8761879ffe22de30d263d7d8ecdf9eaa3ba62468
Daniel Bastos <danielfloresbastos@gmail.com>
2017-08-03 09:11:59 -0300

    add test

--- tests/test_tapioca_bookingsync.py
+++ tests/test_tapioca_bookingsync.py
@@ -1,13 +1,22 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
+import os
 import unittest
+
 from tapioca_bookingsync import BookingSync
 
 
-class TestTapiocaBookingsync(unittest.TestCase):
-
+class TestBookingSyncTapioca(unittest.TestCase):
     def setUp(self):
-        self.wrapper = BookingSync()
+        self.api_client = BookingSync(
+            client_id=os.getenv('BOOKINGSYNC_CLIENT_ID', default=''),
+            access_token=os.getenv('BOOKINGSYNC_ACCESS_TOKEN', default=''),
+        )
+
+    def test_resource_access(self):
+        resource = self.api_client.rentals()
+        import ipdb; ipdb.set_trace()
+        assert resource.data == 'https://www.bookingsync.com/api/v3/rentals'
 
 
 if __name__ == '__main__':
