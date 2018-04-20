c8ec1b66a88a71061e16c1604d59790bf7ca293f
Daniel Bastos <danielfloresbastos@gmail.com>
2018-04-20 16:44:52 -0300

    replace quotation marks duple to simple quotation marks

--- tapioca_bookingsync/resource_mapping.py
+++ tapioca_bookingsync/resource_mapping.py
@@ -2,77 +2,77 @@ RESOURCE_MAPPING = {
     'accounts': {
         'resource': 'accounts/',
         'docs': 'http://developers.bookingsync.com/reference/endpoints/accounts/#list-accounts',
-        "methods": [
-            "GET",
+        'methods': [
+            'GET',
         ]
     },
     'account': {
         'resource': 'accounts/{account_id}',
         'docs': 'http://developers.bookingsync.com/reference/endpoints/accounts/#get-a-single-account',
-        "methods": [
-            "GET",
+        'methods': [
+            'GET',
         ]
     },
     'clients': {
         'resource': 'clients/',
         'docs': 'http://developers.bookingsync.com/reference/endpoints/clients/#list-clients',
-        "methods": [
-            "GET",
+        'methods': [
+            'GET',
         ]
     },
     'client': {
         'resource': 'clients/{client_id}',
         'docs': 'http://developers.bookingsync.com/reference/endpoints/clients/#get-a-single-client',
-        "methods": [
-            "GET",
+        'methods': [
+            'GET',
         ]
     },
     'rentals': {
         'resource': 'rentals/',
         'docs': 'http://developers.bookingsync.com/reference/endpoints/rentals/#list-rentals',
-        "methods": [
-            "GET",
-            "POST"
+        'methods': [
+            'GET',
+            'POST'
          ]
     },
     'search_rentals': {
         'resource': 'rentals/search/',
         'docs': 'http://developers.bookingsync.com/reference/endpoints/rentals/#search-rentals',
-        "methods": [
-            "GET",
+        'methods': [
+            'GET',
          ]
     },
-    "rental": {
-        "resource": "rentals/{rental_id}",
-        'docs': "http://developers.bookingsync.com/reference/endpoints/rentals/#get-a-single-rental",
-        "methods": [
-            "GET",
-            "PUT",
-            "DELETE"
+    'rental': {
+        'resource': 'rentals/{rental_id}',
+        'docs': 'http://developers.bookingsync.com/reference/endpoints/rentals/#get-a-single-rental',
+        'methods': [
+            'GET',
+            'PUT',
+            'DELETE'
          ]
     },
     'bookings': {
         'resource': 'bookings/',
         'docs': 'http://developers.bookingsync.com/reference/endpoints/bookings/#list-bookings',
-        "methods": [
-            "GET",
+        'methods': [
+            'GET',
          ]
     },
     'booking': {
         'resource': 'bookings/{booking_id}',
         'docs': 'http://developers.bookingsync.com/reference/endpoints/bookings/#list-bookings',
-        "methods": [
-            "GET",
-            "PUT",
-            "DELETE"
+        'methods': [
+            'GET',
+            'PUT',
+            'DELETE'
         ]
     },
     'inquiry': {
         'resource': 'inquiries/{id}',
-        "methods": [
-            "GET",
-            "PUT",
-            "DELETE"
+        'methods': [
+            'GET',
+            'PUT',
+            'DELETE'
          ],
         'docs': 'http://developers.bookingsync.com/reference/endpoints/inquiries/#get-a-single-inquiry'
     },
@@ -99,9 +99,9 @@ RESOURCE_MAPPING = {
     'amenities': {
         'resource': 'amenities/',
         'docs': 'http://developers.bookingsync.com/reference/endpoints/amenities/',
-        "methods": [
-            "GET",
-            "POST"
+        'methods': [
+            'GET',
+            'POST'
          ]
     },
 }
